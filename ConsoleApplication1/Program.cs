4940fbd8e139fe095c9ba6b7229fc66a26b15f9c
kusachiy <xavierernandez@gmail.com>
2016-12-19 21:49:38 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace Client_Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("That program can transfer small file. I've test up to 850kb file");
+                IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+                // Создаем сокет Tcp/Ip
+                Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                string fileName = "input.txt";
+                string filePath = "";
+                byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+
+                byte[] fileData = File.ReadAllBytes(filePath + fileName);
+                byte[] clientData = new byte[4 + fileNameByte.Length + fileData.Length];
+                byte[] fileNameLen = BitConverter.GetBytes(fileNameByte.Length);
+
+                fileNameLen.CopyTo(clientData, 0);
+                fileNameByte.CopyTo(clientData, 4);
+                fileData.CopyTo(clientData, 4 + fileNameByte.Length);
+
+                sListener.Connect(ipEndPoint);
+                sListener.Send(clientData);
+                Console.WriteLine("File:{0} has been sent.", fileName);
+                sListener.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("File Sending fail." + ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ ServerTCP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+namespace beginSocketServer
+{
+    //FILE TRANSFER USING C#.NET SOCKET PROGRAMMING - SERVER
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("That program can transfer small file. I've test up to 850kb file");
+                IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+                // Создаем сокет Tcp/Ip
+                Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(100);
+                Socket clientSock = sListener.Accept();
+
+                byte[] clientData = new byte[1024 * 5000];
+                string receivedPath = "d:/";
+
+                int receivedBytesLen = clientSock.Receive(clientData);
+                int fileNameLen = BitConverter.ToInt32(clientData, 0);
+                string fileName = Encoding.ASCII.GetString(clientData, 4, fileNameLen);
+                Console.WriteLine("Client:{0} connected & File {1} started received.", clientSock.RemoteEndPoint, fileName);
+                BinaryWriter bWrite = new BinaryWriter(File.Open(receivedPath + fileName, FileMode.Append)); ;
+                bWrite.Write(clientData, 4 + fileNameLen, receivedBytesLen - 4 - fileNameLen);
+                Console.WriteLine("File: {0} received & saved at path: {1}", fileName, receivedPath);
+
+                bWrite.Close();
+                clientSock.Close();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("File Receiving fail." + ex.Message);
+            }
+        }
+}
+}
